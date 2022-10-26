b8f2d634a5e7aec2bb5fc460ea29549db0bc52da
Eduard <dirol2016@list.ru>
2022-10-26 15:59:55 +0500

    DZ_1

---
+++ Exercise_1.java
@@ -0,0 +1,25 @@
+package DZ_1;
+
+import java.util.Arrays;
+
+public class Exercise_1 {
+    public static void main(String[] args) {
+        int[] arr1 = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int[] arr2 = {9, 8, 7, 6, 5, 4, 3, 2, 1};
+        int[] result = difOfArr(arr1, arr2);
+        System.out.println(Arrays.toString(result));
+    }
+    private static int[] difOfArr(int[] arr1, int[] arr2) {
+        if (arr1 == null || arr2 == null){
+            throw new RuntimeException("Some array is null");
+        }
+        if (arr1.length != arr2.length){
+            throw new RuntimeException("Length of arrays not equal");
+        }
+        int[] arr = new int[arr1.length];
+        for (int i = 0; i < arr1.length; i++) {
+            arr[i] = arr1[i] - arr2[i];
+        }
+        return arr;
+    }
+}
